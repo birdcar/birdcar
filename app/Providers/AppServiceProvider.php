98fa99ceb57d9172a6516e01cefaf5d37231bf00
Birdcar <434063+birdcar@users.noreply.github.com>
2026-09-01 09:35:40 -0500

    Chore: Install the dependencies I like to use for a Laravel buildout

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Illuminate\Foundation\DevCommands;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
@@ -24,6 +25,10 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->configureDefaults();
+        if (app()->environment('local')) {
+            // Use the Reverb process Herd provides
+            DevCommands::except('reverb');
+        }
     }
 
     /**
